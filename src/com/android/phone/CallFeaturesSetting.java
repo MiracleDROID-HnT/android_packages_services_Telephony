19ae5a649a4245eadab5cd47b2894b4a10353c5a
LakshyaTaneja <taneja.lakshay5@gmail.com>
2018-04-16 13:58:24 +0000

    Rebrand
    Signed-off-by: LakshyaTaneja <taneja.lakshay5@gmail.com>

--- src/com/android/phone/CallFeaturesSetting.java
+++ src/com/android/phone/CallFeaturesSetting.java
@@ -65,7 +65,7 @@ import com.android.phone.settings.VoicemailSettingsActivity;
 import com.android.phone.settings.fdn.FdnSetting;
 import com.android.services.telephony.sip.SipUtil;
 
-import com.android.internal.util.elixir.ElixirUtils;
+import com.android.internal.util.mdroid.MDroidUtils;
 
 import java.lang.String;
 import java.util.ArrayList;
